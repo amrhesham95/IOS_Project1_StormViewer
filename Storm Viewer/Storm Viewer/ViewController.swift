b57e2454986f0c1911777b44ad543e39eba401c4
Amr Hesham <amr.hesham@fawry.com>
2020-01-24 08:55:54 +0200

    enhanced the project with using threads (perform selectors )

--- Storm Viewer/Storm Viewer/ViewController.swift
+++ Storm Viewer/Storm Viewer/ViewController.swift
@@ -17,17 +17,10 @@ class ViewController: UITableViewController {
         //according to apple guidlines this should be done in the first screen only so we have to disable it in the details screen
         navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view.
-        let fm=FileManager.default
-        let path=Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
         
-        for item in items{
-            if item.hasPrefix("nssl"){
-                pictures.append(item)
-            }
-        }
-        pictures = pictures.sorted()
-        print(pictures)
+        // load the data in background thread
+        performSelector(inBackground: #selector(loadDataFromTheFile), with: nil)
+        
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
@@ -47,6 +40,22 @@ class ViewController: UITableViewController {
             self.navigationController?.pushViewController(detailViewController, animated: true)
         }
         
+    }
+    
+    @objc func loadDataFromTheFile(){
+        let fm=FileManager.default
+        let path=Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items{
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+        // here we are in background thread so we aren't supposed to access UI in here so the next line to access the UI from the main thread
+        self.tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        print(pictures)
     }
 }
 
